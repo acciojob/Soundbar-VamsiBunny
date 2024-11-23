9606402eaa8d6bf00206bd8582c8adb3d66abf44
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-23 19:43:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+let currentSound = null;
+let audio = new Audio();
+
+function playSound(filename) {
+  // If there's a sound already playing, stop it
+  if (currentSound !== null) {
+    audio.pause();
+  }
+  
+  // Set the new sound source and play it
+  audio = new Audio('sounds/' + filename);
+  audio.play();
+  currentSound = filename;
+}
+
+function stopSound() {
+  if (audio) {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+  currentSound = null;
+}
